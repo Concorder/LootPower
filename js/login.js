0231c02a7cdab2f9418713f3310db4a2f294910f
Concorder <arenko.design@gmail.com>
2021-08-05 15:39:29 +0300

    contactButton added

--- js/login.js
+++ js/login.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-
     var ifLogged = false
     var clicked = 0;
     let profIcon = $('#profile');
     if (!ifLogged) showSubmenu();
-
     function showSubmenu() {
         function stopDefAction(evt) {
             evt.preventDefault();
@@ -21,8 +19,19 @@ $(document).ready(function(){
                 clicked = 0;
             }
          });
-
     }
-
-
+// contact button
+    let placementDiv = document.getElementsByTagName('body')[0];
+    console.log(placementDiv.dataset);
+    //add data-nobutton="1" in body to hide
+    if (placementDiv.dataset.nobutton !== "1"
+        && !( document.URL.includes("game_play"))
+        && !( document.URL.includes("contactus")))
+    {
+        var newButton = document.createElement('a');
+        newButton.setAttribute("id","buttonCode");
+        newButton.setAttribute("href","contactus.html");
+        newButton.innerHTML = "<span>Have a problem?</span>"
+        placementDiv.prepend(newButton);
+    }
 })
